9fae636aecfc75aaf43352822422b66d5eadfa09
cleo <2285976746@qq.com>
2015-09-25 22:37:11 -0400

    ch3,ch4 updated

--- ch4/Solution11.java
+++ ch4/Solution11.java
@@ -1,39 +1,54 @@
 package ch4;
 
+import java.util.Random;
+
 /**
  * Created by apple on 9/24/15.
  */
 public class Solution11 {
 
-    class treeNode{
-        int val;
-        int size;
-        TreeNode left;
-        TreeNode right;
-        TreeNode parent;
-        public treeNode(int x){
-            this.val=x;
+
+
+    class tree{
+        TreeNode root=null;
+
+
+        public TreeNode getRandomNode(){
+            if (root==null) return null;
+            Random random = new Random();
+            int i = random.nextInt();
+            return root.getNode(i);
         }
 
-        public int size(){
-            return size;
+
+
+        public class treeNode{
+            int val;
+            int size;
+            TreeNode left;
+            TreeNode right;
+            TreeNode parent;
+            public treeNode(int x){
+                this.val=x;
+            }
+
+            public int size(){
+                return (root==null)?0:root.size;
+
+            }
+            public treeNode getNode(int i){
+                int leftSize;
+                if (left==null) leftSize=0;
+                else leftSize=left.size;
+
+                if (i<leftSize) return left.getNode(i);
+                else if (i==leftSize) return this;
+                else return right.getNode(i-(leftSize+1));
+            }
+
+
         };
-
-        public treeNode getNode(int i){
-            int leftSize;
-            if (left==null) leftSize=0;
-            else leftSize=left.size;
-
-            if (i<leftSize) return left.getNode(i);
-            else if (i==leftSize) return this;
-            else return right.getNode(i-(leftSize+1));
-        }
-    }
-
-    public void getRandomNode(){
-
-    }
-
+    };
 
 
 
