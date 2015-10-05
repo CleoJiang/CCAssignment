f92c3a7255d0aa57dfec3ff5ae56dd1ecb953b85
cleo <2285976746@qq.com>
2015-10-05 11:25:09 -0400

    ch3,ch4 updated

--- ch4/Solution09.java
+++ ch4/Solution09.java
@@ -1,8 +1,67 @@
 package ch4;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+
 /**
  * Created by apple on 9/23/15.
  */
 public class Solution09{
+
+    ArrayList<LinkedList<Integer>> BSTsequence(TreeNode N){
+        ArrayList<LinkedList<Integer>> result = new ArrayList<LinkedList<Integer>>();
+
+        // deal with empty tree
+        if (N==null){
+            result.add(new LinkedList<Integer>());
+            return result;
+        }
+
+        LinkedList<Integer> pre = new LinkedList<Integer>();
+        pre.add(N.val);
+
+        ArrayList<LinkedList<Integer>> left = BSTsequence(N.left);
+        ArrayList<LinkedList<Integer>> right = BSTsequence(N.right);
+
+        for(LinkedList<Integer> leftone:left){
+            for(LinkedList<Integer> rightone: right){
+                ArrayList<LinkedList<Integer>> weave = new ArrayList<LinkedList<Integer>>();
+                weaveList(leftone,rightone,weave,pre);
+                result.addAll(weave);
+            }
+        }
+
+        return result;
+    }
+
+    void weaveList(LinkedList<Integer> first, LinkedList<Integer> sec,
+                   ArrayList<LinkedList<Integer>> results , LinkedList<Integer> pre){
+        if (first.size()==0 || sec.size()==0){
+            LinkedList<Integer> result = (LinkedList<Integer>) pre.clone();
+            result.addAll(first);
+            result.addAll(sec);
+            results.add(result);
+            return;
+        }
+
+        int headfirst = first.removeFirst();
+        pre.addLast(headfirst);
+        pre.removeLast();
+        first.addFirst(headfirst);
+
+        int headsec = sec.removeLast();
+        pre.addLast(headsec);
+        weaveList(first,sec,results,pre);
+        pre.removeLast();
+        sec.addFirst(headsec);
+
+
+    }
+
+
+
+
+
+
 
 }
