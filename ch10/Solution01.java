7164026660173e5c572923c89a35e19659e6c159
cleo <2285976746@qq.com>
2015-10-05 11:26:42 -0400

    ch3,ch4 updated

--- ch10/Solution01.java
+++ ch10/Solution01.java
@@ -4,4 +4,41 @@ package ch10;
  * Created by apple on 9/25/15.
  */
 public class Solution01 {
+
+    public static void main(String[] args) {
+        int[] longer = new int[8];
+        longer[0]=2; longer[1]=4; longer[2]=6; longer[3]=7; longer[4]=8;
+        int[] shorter = {1,3,5};
+        merge(longer,shorter);
+        System.out.println(longer);
+
+    }
+
+
+    // from the end of two string, compare each element one by one.
+    // put the bigger one into the end of the longer string to form the new sorted array
+    public static void merge(int[] a, int[] b){
+        int aEnd = a.length-1;
+        int bEnd = b.length-1;
+        int mergeEnd = aEnd + bEnd - 1;
+
+        while(bEnd>=0){
+            if(aEnd>=0 && a[aEnd]>b[bEnd]){
+                a[mergeEnd]=a[aEnd];
+                aEnd--;
+            }else {
+                a[mergeEnd]=b[bEnd];
+                bEnd--;
+            }
+            mergeEnd--;
+        }
+
+
+
+
+
+
+
+    }
+
 }
