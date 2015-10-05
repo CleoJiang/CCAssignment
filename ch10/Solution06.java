da8dee97b3ea1d751e23f071dc45aec2b8da742e
cleo <2285976746@qq.com>
2015-10-05 11:30:35 -0400

    10.6

---
+++ ch10/Solution06.java
@@ -0,0 +1,13 @@
+package ch10;
+
+/**
+ * Created by apple on 9/27/15.
+ */
+public class Solution06 {
+}
+//Use external sort
+// divide the whole data set into small trunks. the size of trunk is equal to size of our memory
+//such that we could process it in the memory
+//After separately sort each trunks, we merge them one by one.
+//at last get the fully sorted file.
+
