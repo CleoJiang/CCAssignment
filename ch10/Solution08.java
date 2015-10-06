49a57b33e953e0f740e39a26e3a52cac7f5a625a
cleo <2285976746@qq.com>
2015-10-05 20:34:42 -0400

    10.3

--- ch10/Solution08.java
+++ ch10/Solution08.java
@@ -6,9 +6,14 @@ package ch10;
  * Created by apple on 9/27/15.
  */
 public class Solution08 {
+
+    // use a int array as a bit vector to store existence for all 32000 different Integer.
+
     void checkDuplicate(int[] array){
         BitSet bitSet = new BitSet(32000);
 
+        // if this number already appeared, print it out
+        // else record its existence in the bit vector
         for (int num:array){
             int numbit = num-1;
             if (bitSet.get(numbit)) System.out.println(num);
