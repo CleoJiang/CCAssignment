674a06d3756b01f0f8364b8f385cc1f16ec0f738
cleo <2285976746@qq.com>
2015-10-05 20:32:20 -0400

    10.8

---
+++ ch10/Solution08.java
@@ -0,0 +1,40 @@
+package ch10;
+
+
+
+/**
+ * Created by apple on 9/27/15.
+ */
+public class Solution08 {
+    void checkDuplicate(int[] array){
+        BitSet bitSet = new BitSet(32000);
+
+        for (int num:array){
+            int numbit = num-1;
+            if (bitSet.get(numbit)) System.out.println(num);
+            else bitSet.set(numbit);
+        }
+    }
+
+    private class BitSet{
+        int[] bitset;
+        BitSet(int size){
+            bitset = new int[size>>5+1];
+        }
+        public boolean get(int index){
+            int word = index >> 5;
+            int bit = index & 0x1F;
+            return (bitset[word] & (1<<bit))!=0;
+        }
+        public void set(int index){
+            int word =index >> 5;
+            int bit = index & 0x1F;
+            bitset[word] = bitset[word] | (1<<bit);
+        }
+
+    };
+
+
+
+
+}
