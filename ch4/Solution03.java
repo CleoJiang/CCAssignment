18979f70a83ae5ec319ef2a7f4e1ec82d4aa30e7
cleo <minhuij@andrew.cmu.edu>
2015-11-02 23:14:12 -0500

    ch4

--- ch4/Solution03.java
+++ ch4/Solution03.java
@@ -2,27 +2,39 @@ package ch4;
 
 import java.util.ArrayList;
 import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
 
 /**
  * Created by apple on 9/23/15.
  */
 public class Solution03 {
 
-    ArrayList<LinkedList<TreeNode>> levelTravel(TreeNode root){
-        ArrayList<LinkedList<TreeNode>> results = new ArrayList<LinkedList<TreeNode>>();
-        LinkedList<TreeNode> c = new LinkedList<TreeNode>();
-        if (root!=null) c.add(root);
+    List<List<Integer>> levelTravel(TreeNode root){
+        List<List<Integer>> result = new ArrayList<List<Integer>>();
+        Queue<TreeNode> queue = new LinkedList<TreeNode>();
 
-        while(c.size()!=0){
-            results.add(c);
-            LinkedList<TreeNode> upperNodes = c;
-            c = new LinkedList<TreeNode>();
-            for (TreeNode upperNode: upperNodes){
-                if (upperNode.left!=null) c.add(upperNode.left);
-                if (upperNode.right!=null) c.add(upperNode.right);
+        queue.add(root);
+        while(!queue.isEmpty()){
+            List<Integer> level = new ArrayList<Integer>();
+            int size = queue.size();
+
+            for(int i = 0; i < size; i++){
+                TreeNode node = queue.peek();
+                queue.remove();
+                level.add(node.val);
+                if (node.left != null){
+                    queue.add(node.left);
+                }
+                if (node.right != null){
+                    queue.add(node.right);
+                }
+
             }
+            result.add(level);
         }
-        return results;
+
+        return result;
 
     }
 
